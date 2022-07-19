24732a520b7c97778bd78729026767f0f6457712
Yaroslav Kovalev <yakovaldos@yandex.ru>
2022-07-19 20:20:08 +0300

    feat: добавляет range slider

--- source/js/modules/init-range-slider.js
+++ source/js/modules/init-range-slider.js
@@ -1,16 +1,33 @@
+/* eslint-disable no-invalid-this */
 // Range slider on rent-catalog filter -- Start --
 import noUiSlider from 'nouislider';
 
 const initRangeSlider = () => {
   let priceRange = document.getElementById('noui-1');
+
   if (priceRange) {
+
+    const input0 = document.getElementById('price-from');
+    const input1 = document.getElementById('price-to');
+    const inputs = [input0, input1];
+
     noUiSlider.create(priceRange, {
-      start: [10, 30],
+      start: [35000, 55000],
       connect: true,
       range: {
-        'min': -20,
-        'max': 40,
+        'min': 30000,
+        'max': 60000,
       },
+    });
+
+    priceRange.noUiSlider.on('update', function (values, handle) {
+      inputs[handle].value = values[handle];
+    });
+    input0.addEventListener('change', function () {
+      priceRange.noUiSlider.set([this.value, null]);
+    });
+    input1.addEventListener('change', function () {
+      priceRange.noUiSlider.set([null, this.value]);
     });
   }
 };
