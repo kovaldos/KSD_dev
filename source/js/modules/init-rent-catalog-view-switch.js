ddbbd16b951db8da13e98377a37c0e685b0adec0
Yaroslav Kovalev <yakovaldos@yandex.ru>
2022-08-09 21:17:23 +0300

    feat: добавляет страницу Продажи Каталог

--- source/js/modules/init-rent-catalog-view-switch.js
+++ source/js/modules/init-rent-catalog-view-switch.js
@@ -1,40 +1,40 @@
 // Переключения вида в каталоге аренды -- Начало --
-// Switch view in rent-catalog -- Start --
+// Switch view in catalog -- Start --
 const initRentCatalogViewSwitch = () => {
-  const catalogWrap = document.querySelector('.rent-catalog');
+  const catalogWrap = document.querySelector('.catalog');
   if (catalogWrap) {
-    const btnStripes = catalogWrap.querySelector('.rent-catalog__nav-btn--stripes');
-    const btnSquares = catalogWrap.querySelector('.rent-catalog__nav-btn--squares');
-    const cardsWrapper = catalogWrap.querySelector('.rent-catalog__cards');
+    const btnStripes = catalogWrap.querySelector('.catalog__nav-btn--stripes');
+    const btnSquares = catalogWrap.querySelector('.catalog__nav-btn--squares');
+    const cardsWrapper = catalogWrap.querySelector('.catalog__cards');
     catalogWrap.addEventListener('click', (e) => {
       let target = e.target;
       if (catalogWrap.offsetWidth > 1023 && target === btnStripes) {
         btnStripes.classList.remove('is-active');
         btnSquares.classList.add('is-active');
-        cardsWrapper.classList.add('rent-catalog__cards--grid');
-        cardsWrapper.classList.remove('rent-catalog__cards--rows');
+        cardsWrapper.classList.add('catalog__cards--grid');
+        cardsWrapper.classList.remove('catalog__cards--rows');
       }
       if (catalogWrap.offsetWidth <= 1023 && target === btnStripes) {
         btnStripes.classList.toggle('is-active');
         btnSquares.classList.toggle('is-active');
-        cardsWrapper.classList.add('rent-catalog__cards--grid');
-        cardsWrapper.classList.remove('rent-catalog__cards--rows');
+        cardsWrapper.classList.add('catalog__cards--grid');
+        cardsWrapper.classList.remove('catalog__cards--rows');
       }
       if (catalogWrap.offsetWidth > 1023 && target === btnSquares) {
         btnSquares.classList.remove('is-active');
         btnStripes.classList.add('is-active');
-        cardsWrapper.classList.remove('rent-catalog__cards--grid');
-        cardsWrapper.classList.add('rent-catalog__cards--rows');
+        cardsWrapper.classList.remove('catalog__cards--grid');
+        cardsWrapper.classList.add('catalog__cards--rows');
       }
       if (catalogWrap.offsetWidth <= 1023 && target === btnSquares) {
         btnSquares.classList.toggle('is-active');
         btnStripes.classList.toggle('is-active');
-        cardsWrapper.classList.remove('rent-catalog__cards--grid');
-        cardsWrapper.classList.add('rent-catalog__cards--rows');
+        cardsWrapper.classList.remove('catalog__cards--grid');
+        cardsWrapper.classList.add('catalog__cards--rows');
       }
     });
   }
 };
 export {initRentCatalogViewSwitch};
 // Переключения вида в каталоге аренды -- Конец --
-// Switch view in rent-catalog -- End --
+// Switch view in catalog -- End --
